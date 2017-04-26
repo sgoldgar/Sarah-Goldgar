b45c7c6f09de0265e25c3501fbde68c9009f6990
Sarah Goldgar <sgoldgar@gmail.com>
2017-04-26 16:33:44 -0500

    added changeable content on about

--- src/components/About.js
+++ src/components/About.js
@@ -2,21 +2,48 @@ import React, {Component} from 'react';
 import '../App.css';
 import './About.css';
 
+function showWebDev(){
+  document.getElementById('webdev').style.display='block';
+  document.getElementById('law').style.display='none';
+  document.getElementById('film').style.display='none';
+
+}
+
+function showLaw(){
+  document.getElementById('law').style.display='block';
+  document.getElementById('webdev').style.display='none';
+  document.getElementById('film').style.display='none';
+}
+
+function showFilm(){
+  document.getElementById('film').style.display='block';
+  document.getElementById('law').style.display='none';
+  document.getElementById('webdev').style.display='none';
+}
+
 class About extends Component{
   render(){
     return(
       <section className="about">
         <div className="flex-row experience">
           <div className="container">
-            <div className="circle"><a>Web Dev</a></div>
-            <div className="circle"><a>Law</a></div>
-            <div className="circle"><a>Film</a></div>
+            <div className="circle">
+              <a onClick={showWebDev}>Web Dev</a>
+            </div>
+            <div className="circle">
+              <a onClick={showLaw}>Law</a>
+            </div>
+            <div className="circle">
+              <a onClick={showFilm}>Film</a>
+            </div>
             <div className="circle"><a target="_blank" href="../public/resume.html">Resume</a></div>
           </div>
-        <div className="row">
-          <p className="more-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        </div>
+
+        <div style={{display:'none'}} id="webdev">This is the lightbox content for webdev.</div>
+        <div style={{display:'none'}} id="law">This is the lightbox content for law.</div>
+        <div style={{display:'none'}} id="film">This is the lightbox content for film.</div>
+
       </section>
     )
   }
