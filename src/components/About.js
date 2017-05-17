b21a6b9c2c10d8ed7ac12334f2f20243cfed0297
Sarah Goldgar <sgoldgar@gmail.com>
2017-05-16 21:08:56 -0500

    fixed rotating about content

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import '../App.css';
 import './About.css';
 import AboutContent from './AboutContent.js';
-// import Resume from './img/Sarah_Goldgar-Resume.pdf'
+import Resume from './img/Sarah_Goldgar-Resume.pdf'
 
 
 class About extends Component{
@@ -11,44 +11,54 @@ class About extends Component{
     this.state = {
       rotateAbout : [
         {
-          tag: 'Front-end web developer with full stack knowledge.',
-          intro: 'Proficient in:',
-          description: 'Languages: HTML • CSS • Javascript • jQuery • C++ • Sass • Python • ES6 Frameworks: Node • React • React-Native • Angular • Express Databases: MongoDB • Firebase Other: Mongoose • React Router • Creating RESTful Apps • Auth0 • oAuth Soft Skills: Agile • Photoshop'
+          id: 'personal-about',
+          tag:'',
+          description: 'I\'m an Austin native who left to pursue a film degree, returned with a law degree, and now am focused on growing my skills and career in front-end web development. I have a dog named Virginia Woof and we like to go on hikes and hang out in the backyard. In my spare time I take pictures, play RPGs and board games, and try my hand at pixel art and css animations.'
         },
         {
+          id: 'webdev',
+          tag: 'Front-end web developer with full stack knowledge.',
+          description: 'HTML • CSS • Javascript • jQuery • C++ • Sass • Python • ES6 • Node • React • React-Native • Angular • Express • MongoDB • Firebase • Mongoose • React Router • REST Apps • Auth0 • oAuth • Agile • Photoshop'
+        },
+        {
+          id: 'law',
           tag: 'Former entertainment and intellectual property attorney.',
-          intro: 'Specializations:',
           description: 'Copyright Registration and Protection • Trademark Protection and Registration • DMCA • Small Business Organization • LLC and Non-profit Creation • Sponsorship, Independent Contractor, Employment, and Venue Rental Agreements'
         },
         {
-          title: 'Formally trained in film production.',
-          intro: 'Trained in:',
+          id: 'film',
+          tag: 'Formally trained in film production.',
           description: 'Producing small projects • Screenwriting • Final Cut Pro • Avid • Technical Operations • Film theory and design'
+        },
+        {
+          id: 'resume',
+          tag: <a href={Resume} download><i className="fa fa-download fa-5x" aria-hidden="true"></i></a>,
+          description: <a href={Resume} download>Download a copy of my resume</a>
         }
       ],
-      currentRotate: null,
-      resumeClicked: false
+      currentRotate: 0
     }
   }
   render(){
     return(
       <section className="about">
 
-        <AboutContent aboutContentData={this.state.rotateAbout[this.state.currentRotate]}
-        />
+        <AboutContent aboutContentData={this.state.rotateAbout[this.state.currentRotate]}/>
 
         <div className="flex-row experience">
           <div className="container">
-            <div className="circle" onClick={() => {this.setState({currentRotate:0, resumeClicked: false})}}>
+            <div className="circle" onClick={() => {this.setState({currentRotate:1})}}>
               <p>Web Dev</p>
             </div>
-            <div className="circle" onClick={() => {this.setState({currentRotate:1, resumeClicked:false})}}>
+            <div className="circle" onClick={() => {this.setState({currentRotate:2})}}>
               <p>Law</p>
             </div>
-            <div className="circle" onClick={() => {this.setState({currentRotate:2, resumeClicked:false})}}>
+            <div className="circle" onClick={() => {this.setState({currentRotate:3})}}>
               <p>Film</p>
             </div>
-            <div className="circle" onClick={() => {this.setState({resumeClicked:true})}}><p>Resume</p></div>
+            <div className="circle" onClick={() => {this.setState({currentRotate:4})}}>
+              <p>Resume</p>
+            </div>
           </div>
         </div>
 
--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import '../App.css';
+import About from './About.js';
 import './About.css';
-import Resume from './img/Sarah_Goldgar-Resume.pdf'
+import Resume from './img/Sarah_Goldgar-Resume.pdf';
+
 
 
 class AboutContent extends Component{
@@ -9,33 +11,12 @@ class AboutContent extends Component{
     super(props);
   }
   render(){
-
-    if (this.props.currentRotate == null) {
       return (
-        <div id="personal-about">
-          <p>I'm an Austin native who left to pursue a film degree, returned with a law degree, and now am focused on growing my skills and career in front-end web development. I have a dog named Virginia Woof (Ginny for short-- click her picture in the nav bar for some cute pictures) and we like to go on hikes and hang out in the backyard (I read sci-fi and mysteries and she digs up my hard work in the garden). In my spare time I take pictures, play RPGs and board games, and try my hand at pixel art and css animations.
-          </p>
+        <div id={this.props.aboutContentData.id}>
+          <p><strong>{this.props.aboutContentData.tag}</strong></p>
+          <p>{this.props.aboutContentData.description}</p>
         </div>
       )
-    }
-    else if (this.props.resumeClicked){
-      return(
-        <div style={{display:'none'}} id="resume">
-            <a href={Resume} download><i className="fa fa-download fa-5x" aria-hidden="true"></i></a>
-            <a href={Resume} download>Download a copy of my resume</a>
-        </div>
-      )
-    }
-    else {
-      return(
-          <div style={{display:'none'}}>
-            <p><strong>{this.props.aboutContentData.tag}</strong></p>
-            <p>this.props.aboutContentData.intro</p>
-            <p>{this.props.aboutContentData.description}</p>
-          </div>
-      )
-    }
-
   }
 }
 
